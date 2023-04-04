075197ccc9a1204fd7cf17f2f4aba90917b84c8c
VadVergasov <vadim.vergasov2003@gmail.com>
2023-04-04 14:06:25 +0300

    Added weight edge

---
+++ src/graph/weight_edge.cpp
@@ -0,0 +1,7 @@
+/*
+WeightEdge  Copyright (C) 2023  VadVergasov
+This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type `show c' for details.
+*/
+#include "graph/weight_edge.h"
