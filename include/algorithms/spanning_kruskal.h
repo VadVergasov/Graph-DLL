d33428e3befdc7d44d0121fa07cb03c15d4fd1fd
VadVergasov <vadim.vergasov2003@gmail.com>
2023-04-04 14:23:08 +0300

    Added spanning kruskal

---
+++ include/algorithms/spanning_kruskal.h
@@ -0,0 +1,17 @@
+/*
+Spanning Tree by Kruskal algorithm  Copyright (C) 2023  VadVergasov
+This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type `show c' for details.
+*/
+#pragma once
+
+#include "graph/igraph.h"
+
+namespace graph{
+
+class SpanningKruskal {
+
+};
+
+};
---
+++ src/algorithms/spanning_kruskal.cpp
@@ -0,0 +1,7 @@
+/*
+Spanning Tree by Kruskal algorithm  Copyright (C) 2023  VadVergasov
+This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type `show c' for details.
+*/
+#include "algorithms/spanning_kruskal.h"
