7b3b7bba6c6a422b6c9b2879af5e51ec9b746977
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-05-14 23:26:38 +0300

    Implemented removing neigbor in vertex

--- src/graph/ivertex.cpp
+++ src/graph/ivertex.cpp
@@ -6,13 +6,35 @@ under certain conditions; type `show c' for details.
 */
 #include "graph/ivertex.h"
 
+#include "graph/ibidirectional_edge.h"
+
 namespace graph {
 
 void IVertex::RemoveNeighbor(const IVertex& vertex) {
     for (auto it = edges_.begin(); it != edges_.end(); it++) {
-        if ((*it)->End().GetId() == vertex.GetId()) {
-            edges_.erase(it);
-            break;
+        const auto edge = *it;
+        if (auto bidirectional_edge =
+                std::dynamic_pointer_cast<const IBidirectionalEdge>(edge)) {
+            auto start = bidirectional_edge->Start(), end = edge->End();
+            if (start.GetId() != this->GetId()) {
+                swap(start, end);
+            }
+            if (end.GetId() == vertex.GetId()) {
+                for (auto other = end.edges_.begin(); other != end.edges_.end();
+                     other++) {
+                    if (edge == *it) {
+                        end.edges_.erase(it);
+                        break;
+                    }
+                }
+                edges_.erase(it);
+                break;
+            }
+        } else {
+            if (edge->End().GetId() == vertex.GetId()) {
+                edges_.erase(it);
+                break;
+            }
         }
     }
 }
